7162d783da029c5db13bcb5e63f3751953fcefd4
Chris Reynolds <chris@csjt.co.uk>
2022-04-15 11:35:07 +0100

    Version that reads all NA files and extracts the images to folders under c:\temp

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-ePerNAFileDump
+# ePerNAFileDump
+
+This utility extracts image files from an installation of the Fiat ePer system.  It has been tested on release 20 of ePer, 
+it is known that later releases of ePer hold their images differently, for instance release 84 holds the images in `res` files 
+that are actually zip files.
+
+The utility will scan all `NA` files in the ePer installation and write the images out to sub-folders under `c:\Temp`.
+It is left as an exercise for the reader to change this if required.
+
+## Background
+
+For release 20 of ePer the images of parts are held in NA files in folder `C:\Program Files (x86)\Fiat\ePER\data\SP.NA.00900.FCTLR`
+(assuming a standard installation).
+
+The filenames are the make code concatenated with the catalog code so 'FPK' is the Fiat Barchetta.  Each file contains a
+header block follwed by the images in PNG format.  The structure of the NA file is described below.
+
+## Format of an NA file
+
+All integers little-endian unless otherwise specified.
+
+### File header
+
+- 16 bit integer of unknown purpose
+- 16 bit integer giving number of entries in the directory
+- Array of directory entries (see below for format)
+- Blank section
+- PNG files concatenated together
+
+### Directory entry
+
+- 16 bit integer looks to be a counter
+- 10 bytes of image name
+- 32 bit integer offset of main image
+- 32 bit integer length of main image
+- 32 bit integer offset of thumbnail
+- 32 bit integer length of thumbnail
--- ePerNAFileDump/Program.cs
+++ ePerNAFileDump/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,48 @@ namespace ePerNAFileDump
     {
         static void Main(string[] args)
         {
+            var files = Directory.GetFiles(@"C:\Program Files (x86)\Fiat\ePER\data\SP.NA.00900.FCTLR", "*.na");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+                ProcessNaFile(file);
+            }
+        }
+        static void ProcessNaFile(string fileName)
+        {
+            var folder = $"C:\\temp\\eperimages\\{Path.GetFileNameWithoutExtension(fileName)}";
+            if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+            var reader = new BinaryReader(File.OpenRead(fileName));
+            reader.ReadInt16();
+            Int32 numberOfEntries = reader.ReadInt16();
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                Int16 imageIndex = reader.ReadInt16();
+                byte[] imageNameBytes = reader.ReadBytes(10);
+                string imageName = System.Text.Encoding.ASCII.GetString(imageNameBytes);
+                Int32 mainImageStart = reader.ReadInt32();
+                Int32 mainImageLength = reader.ReadInt32();
+                Int32 thumbImageStart = reader.ReadInt32();
+                Int32 thumbImageLength = reader.ReadInt32();
+                Console.WriteLine($"{imageIndex} {imageName} {mainImageStart} {mainImageLength} {thumbImageStart} {thumbImageLength}");
+                var pos = reader.BaseStream.Position;
+                reader.BaseStream.Seek(mainImageStart, SeekOrigin.Begin);
+                var imageBytes = reader.ReadBytes(mainImageLength);
+                if (imageBytes[0] != 137 || imageBytes[1] != 80 || imageBytes[2] != 78 || imageBytes[3] != 71)
+                    Console.WriteLine("Not a PNG");
+                using (var output = new BinaryWriter(File.Open(Path.Combine(folder, $"{imageName}.PNG"), FileMode.Create)))
+                {
+                    output.Write(imageBytes);
+                }
+                reader.BaseStream.Seek(thumbImageStart, SeekOrigin.Begin);
+                imageBytes = reader.ReadBytes(thumbImageLength);
+                using (var output = new BinaryWriter(File.Open(Path.Combine(folder, $"{imageName}Thumb.PNG"), FileMode.Create)))
+                {
+                    output.Write(imageBytes);
+                }
+                reader.BaseStream.Seek(pos, SeekOrigin.Begin);
+
+            }
 
         }
     }
